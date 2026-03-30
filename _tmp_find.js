bfd7a9fe099a0f679182713bf12af8e0a0480e8e
我的名字 <123@qq.com>
2026-03-30 13:33:05 +0800

    新页面 - 131

--- _tmp_find.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const s = fs.readFileSync('index.html', 'utf8');
-let i = 0;
-while ((i = s.indexOf('taskDashCol', i + 1)) >= 0) {
-  if (i < 300000) {
-    console.log(i, s.slice(Math.max(0, i - 30), i + 100));
-    console.log('---');
-  }
-}
